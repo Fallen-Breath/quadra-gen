07e4b25140be11699d4d8231a328d12558769067
Fallen_Breath <nellaforax@outlook.com>
2026-09-04 00:01:59 +0800

    init quadra-gen

--- src/main/java/me/fallenbreath/quadragen/mixins/MinecraftServerMixin.java
+++ src/main/java/me/fallenbreath/quadragen/mixins/MinecraftServerMixin.java
@@ -1,24 +1,24 @@
 /*
- * This file is part of the TemplateMod project, licensed under the
+ * This file is part of the Quadra Gen project, licensed under the
  * GNU Lesser General Public License v3.0
  *
  * Copyright (C) 2023  Fallen_Breath and contributors
  *
- * TemplateMod is free software: you can redistribute it and/or modify
+ * Quadra Gen is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * TemplateMod is distributed in the hope that it will be useful,
+ * Quadra Gen is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with TemplateMod.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Quadra Gen.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package me.fallenbreath.template_mod.mixins;
+package me.fallenbreath.quadragen.mixins;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
@@ -32,9 +32,9 @@ public abstract class MinecraftServerMixin
 {
 	@Inject(
 			//#if MC >= 1.16.0
-			//$$ method = "runServer",
+			method = "runServer",
 			//#else
-			method = "run",
+			//$$ method = "run",
 			//#endif
 			at = @At("HEAD")
 	)
